e20cf98157e2e762a1741dff8ee4d47ec70dbbcb
ragheb8778 <81774829+ragheb8778@users.noreply.github.com>
2021-05-03 21:13:53 +0200

    Add files via upload
    git clone https://github.com/ragheb8778/TraceNum.git
    python GetMainInfo.py

---
+++ GetMainInfo.py
@@ -0,0 +1,19 @@
+number = input("Enter the TARGET with <+>: " )
+import phonenumbers
+from number import number
+print(number)
+print("......................")
+
+from phonenumbers import geocoder
+ch_nmber = phonenumbers.parse(number, "CH")
+print(geocoder.description_for_number(ch_nmber,"en"))
+print("......................")
+
+from phonenumbers import carrier
+service_nmber = phonenumbers.parse(number, "RO")
+print(carrier.name_for_number(service_nmber, "en"))
+print("......................")
+print("")
+
+import sys
+sys.exit()
